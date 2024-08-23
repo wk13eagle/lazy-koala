d9dcb7dd7ff449baa6c0f36187b1c9b0f911b1ee
wk13eagle <wk13eagle@126.com>
2024-08-23 17:46:52 +0800

    完善代码注释

--- index.d.ts
+++ index.d.ts
@@ -1,71 +1,180 @@
 import type { AxiosResponse, AxiosInstance } from 'axios'
 
 type GlobalAjaxConfig = {
-  loading?: boolean // 是否启动全局loading
-  failMsg?: boolean // 接口不为成功编码是否弹出失败提示
-  failFn?: boolean // 全局失败逻辑是否启动
-  errMsg?: boolean // 接口发生错误是否弹出错误提示
-  errFn?: boolean // 全局错误处理是否启动
-  query?: Record<string, unknown> // url请求参数, 某些奇怪的post接口需要从query取一部分数据
-  headers?: Record<string, unknown> // headers
+  /**
+   * loading - 是否启动全局loading
+   */
+  loading?: boolean
+
+  /**
+   * failMsg - 接口不为成功编码是否弹出失败提示
+   */
+  failMsg?: boolean
+
+  /**
+   * failFn - 全局失败逻辑是否启动
+   */
+  failFn?: boolean
+
+  /**
+   * errMsg - 接口发生错误是否弹出错误提示
+   */
+  errMsg?: boolean
+
+  /**
+   * errFn - 全局错误处理是否启动
+   */
+  errFn?: boolean
+
+  /**
+   * query - 会追加到url请求参数
+   */
+  query?: Record<string, unknown>
+
+  /**
+   * headers - 会追加到headers中
+   */
+  headers?: Record<string, unknown>
 }
 
 export type AjaxConfig = GlobalAjaxConfig & {
-  id?: string // 请求id
+  /**
+   * id - 请求id, 默认为url, 用来防并发, 如果同一接口同时发起多个, 指定不同id
+   */
+  id?: string
 }
 
+/**
+ * Lazy Get 请求
+ * @param url - 请求地址
+ * @param params - 请求参数
+ * @param config - 可选, 请求配置
+ * @param config.id - 请求id
+ * @param config.loading - 是否启动loading
+ * @param config.failMsg - 接口不为成功编码时是否弹出失败提示
+ * @param config.failFn - 全局失败逻辑是否启动
+ * @param config.errMsg - 接口发生错误时是否弹出错误提示
+ * @param config.errFn - 全局错误处理是否启动
+ * @param config.query - url 请求参数
+ * @param config.headers - 请求头信息
+ * @returns Promise<AxiosResponse>
+ */
 export declare function Get(
-  url?: string,
+  url: string,
   params?: Record<string, unknown>,
   config?: AjaxConfig
 ): Promise<AxiosResponse>
 
+/**
+ * Lazy Post 请求
+ * @param url - 请求地址
+ * @param params - 请求参数
+ * @param config - 可选, 请求配置
+ * @param config.id - 请求id
+ * @param config.loading - 是否启动loading
+ * @param config.failMsg - 接口不为成功编码时是否弹出失败提示
+ * @param config.failFn - 全局失败逻辑是否启动
+ * @param config.errMsg - 接口发生错误时是否弹出错误提示
+ * @param config.errFn - 全局错误处理是否启动
+ * @param config.query - url 请求参数
+ * @param config.headers - 请求头信息
+ * @returns Promise<AxiosResponse>
+ */
 export declare function Post(
-  url?: string,
+  url: string,
   params?: Record<string, unknown>,
   config?: AjaxConfig
 ): Promise<AxiosResponse>
 
+/**
+ * Lazy Axios
+ * @returns AxiosInstance
+ */
 export declare function lazyAxios(): AxiosInstance
 
 interface ResponseConfig {
-  status?: Array<number | string> // 成功状态，可以是数字或字符串
-  codeKeys?: Array<string> // 状态key名，字符串数组
-  msgKeys?: Array<string> // 状态描述key名，字符串数组
+  /**
+   * status - 成功状态编码
+   */
+  status?: Array<number | string>
+
+  /**
+   * codeKeys - 状态key名
+   */
+  codeKeys?: Array<string>
+
+  /**
+   * msgKeys - 状态描述key名
+   */
+  msgKeys?: Array<string>
 }
 
 interface Options {
-  // 等同于axios的baseURL
+  /**
+   * baseURL - 等同于axios的baseURL
+   */
   baseURL?: string
 
-  // 等同于axios的timeout
+  /**
+   * timeout - 等同于axios的timeout
+   */
   timeout?: number
 
-  // 默认配置
+  /**
+   * config - 默认配置
+   */
   config?: GlobalAjaxConfig
 
-  // loading启动
+  /**
+   * loadingStart - loading启动
+   */
   loadingStart?: () => void
 
-  // loading结束
+  /**
+   * loadingEnd - loading结束
+   */
   loadingEnd?: () => void
 
-  // 错误提示
+  /**
+   * errToast - 错误提示
+   */
   errToast?: (msg: string) => void
 
-  // 请求配置
+  /**
+   * requestConfig - 请求配置
+   */
   requestConfig?: (config: any) => any
 
-  // 返回配置
+  /**
+   * responseConfig - 返回配置
+   */
   responseConfig?: ResponseConfig
 
-  // 失败统一处理
+
+  /**
+   * ajaxFail - 失败统一处理
+   */
   ajaxFail?: (res: any) => void
 
-  // 错误统一处理
+  /**
+   * ajaxError - 错误统一处理
+   */
   ajaxError?: (err: any) => void
 }
 
+/**
+ * Lazy Koala 初始化
+ * @param baseURL - 等同于axios的baseURL
+ * @param timeout - 等同于axios的timeout
+ * @param config - 默认配置
+ * @param loadingStart - loading启动
+ * @param loadingStart - loading结束
+ * @param errToast - 错误提示
+ * @param requestConfig - 请求配置
+ * @param responseConfig - 请求配置
+ * @param ajaxFail - 请求配置
+ * @param ajaxError - 请求配置
+ */
 export declare function init(
   options?: Options
 ): void
--- leaf.d.ts
+++ leaf.d.ts
@@ -1,42 +1,196 @@
+/**
+ * Lazy AES加密
+ * 仅支持 'base64' | 'hex'
+ * @param text - 待加密文本
+ * @param key - 密钥
+ * @param type - 可选, 加密方式, 默认 'base64'
+ * @returns 加密后字符串
+ */
 export declare function AES(
   text: string,
   key: string,
   type?: 'base64' | 'hex'
 ): string
+
+/**
+ * Lazy AES解密
+ * 仅支持 'base64' | 'hex'
+ * @param code - 加密后字符串
+ * @param key - 密钥
+ * @param type - 可选, 加密方式, 默认 'base64'
+ * @returns 解密后文本
+ */
 export declare function DAES(
   code: string,
   key: string,
   type?: 'base64' | 'hex'
 ): string
 
+
+/**
+ * Lazy UUID 生成唯一ID
+ * @returns xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+ */
 export declare function UUID(): string
 
+
+/**
+ * Lazy clipboard 复制到剪贴板
+ * @param text - 文本内容
+ * @returns Promise
+ */
+export declare function clipboard(
+  text: string
+): Promise<void>
+
+
+/**
+ * Lazy cookie
+ * 为方便使用, 默认永久有效，如需会话关闭失效, 设置1000或更小数字
+ */
+export declare const cookie: {
+  /**
+   * 设置 cookie
+   * @param name - cookie 的名称
+   * @param value - cookie 的值
+   * @param milliseconds - 可选, cookie 过期时间（以毫秒为单位）, 默认永久有效
+   */
+  setItem(name: string, value: string, milliseconds?: number): void
+
+  /**
+   * 获取 cookie 的值
+   * @param name - cookie 的名称
+   * @returns cookie 的值，如果不存在则返回 null
+   */
+  getItem(name: string): string | null
+
+  /**
+   * 删除指定名称的 cookie
+   * @param name - cookie 的名称
+   */
+  removeItem(name: string): void
+
+  /**
+   * 返回所有 cookie
+   * @returns 包含所有 cookies 的对象，其中键是 cookie 名称，值是 cookie 的值
+   */
+  all(): Record<string, string>
+
+  /**
+   * 清空所有 cookie
+   */
+  clear(): void
+}
+
+
+/**
+ * Lazy inMobile 是否在手机环境
+ * @returns boolean
+ */
 export declare function inMobile(): boolean
+
+/**
+ * Lazy inWX 是否在微信环境
+ * @returns boolean
+ */
 export declare function inWX(): boolean
+
+/**
+ * Lazy inZFB 是否在支付宝环境
+ * @returns boolean
+ */
 export declare function inZFB(): boolean
+
+/**
+ * Lazy inCMCC 是否在移动app环境
+ * @returns boolean
+ */
 export declare function inCMCC(): boolean
 
+
+/**
+ * Lazy formatTime 格式化时间
+ * @param date - 待格式化Date
+ * @param format - 可选, 默认 'YYYY-MM-DD HH:mm:ss'
+ * @returns 2024-08-20 12:00:00
+ */
 export declare function formatTime(
   date: Date,
   format?: string
 ): string
+
+/**
+ * Lazy formatPrice 格式化金额
+ * @param amount - 待格式化金额
+ * @returns 10,000.00
+ */
 export declare function formatPrice(
   amount: number | string,
 ): string
 
+
+/**
+ * Lazy random 随机数
+ */
+export declare const random: {
+  /**
+   * 生成指定区间的随机整数
+   * @param min - 随机数的最小值（包含）
+   * @param max - 随机数的最大值（包含）
+   * @returns 在 min 和 max 之间的随机整数
+   */
+  gen(min: number, max: number): number
+
+  /**
+   * 从数组中随机抽取一个元素
+   * @param array - 要抽取元素的数组
+   * @returns 一个对象，包含随机抽取的元素和其索引
+   * @remarks 如果传入的数组为空，返回null
+   */
+  ext(array?: any[]): {
+    name: unknown
+    index: number
+  } | null
+}
+
+
+/**
+ * Lazy saveFile 保存文件
+ * @param fileData - 文件流
+ * @param fileName - 文件名称
+ */
 export declare function saveFile(
   fileData: ArrayBuffer | Blob,
   fileName: string
 ): void
 
+
+/**
+ * Lazy urlParams 获取url参数
+ * @param key - 可选, 键名
+ * @returns string值或Object对象
+ * @remarks 如果传入key, 返回string, 如果未传入key返回 {}
+ */
 export declare function urlParams(
   key?: string,
 ): string | Object
 
 
+/**
+ * Lazy izMobile 判断手机号
+ * @param mobile - 手机号
+ * @returns boolean
+ */
 export declare function izMobile(
   mobile: string
 ): boolean
+
+/**
+ * Lazy urlParams 获取url参数
+ * @param sms - 验证码
+ * @param len - 可选, 长度, 默认 5
+ * @returns boolean
+ */
 export declare function izSMS(
   sms: string,
   len?: number
