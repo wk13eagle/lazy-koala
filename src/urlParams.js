64406a7e6917db7ac40d7e0ab47fb0a3eaf5f9df
wk13eagle <wk13eagle@126.com>
2024-08-23 17:29:26 +0800

    完善urlParams逻辑

--- src/urlParams.js
+++ src/urlParams.js
@@ -3,7 +3,7 @@ export function urlParams(key) {
   const urlSearchParams = new URLSearchParams(window.location.search)
   const params = Object.fromEntries(urlSearchParams.entries())
   if (key) {
-    return params[key]
+    return params[key] || ''
   } else {
     return params
   }
