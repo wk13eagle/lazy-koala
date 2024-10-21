08da7cc78cfa79341301391eb3fb8f78447632a4
wk13eagle <wk13eagle@126.com>
2024-10-21 10:36:05 +0800

    组件名称小写

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { Loading } from './src/loading'
-import { showToast, showLoadingToast } from './src/toast-n'
+import { showToast, showLoadingToast } from './src/toast'
 
 let loadingId = null
 let lazyKoalaInstance = null
